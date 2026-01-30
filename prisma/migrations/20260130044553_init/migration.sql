463b76294fa53da86e6137ced4797261564ea01f
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2026-01-29 23:48:33 -0500

    fix: section migration

---
+++ prisma/migrations/20260130044553_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Section" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "orderIndex" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Section_pkey" PRIMARY KEY ("id")
+);
